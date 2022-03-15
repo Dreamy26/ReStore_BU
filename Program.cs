11310750880a2240b85db6484a4faa5fbb9109fa
AzureAD\AdryenneWilson <AWilson@autobooks.co>
2022-03-15 12:06:23 -0400

    End of section 2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +13,22 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<StoreContext>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            try
+            {
+               context.Database.Migrate();
+               DbInitializer.Initialize(context);
+            }
+            catch (Exception ex)
+            {
+                 // TODO
+                 logger.LogError(ex, "Problem Migrating DATA!!");
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
